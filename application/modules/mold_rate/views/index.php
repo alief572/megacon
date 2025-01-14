affdf55e44318a040b7847c7684e1a5ced87d7db
alief572 <146901652+alief572@users.noreply.github.com>
2025-01-14 11:54:05 +0700

    Perbaikan - Master Rate Machine - Master Rate Mold
    Penambahan fitur update rate otomatis

--- application/modules/mold_rate/views/index.php
+++ application/modules/mold_rate/views/index.php
@@ -16,6 +16,7 @@ $ENABLE_DELETE  = has_permission('Mold_Rate.Delete');
 	<div class="box-header">
 		<?php if ($ENABLE_VIEW) : ?>
 			<a class="btn btn-success btn-sm add" href="javascript:void(0)" title="Add"><i class="fa fa-plus">&nbsp;</i>Add</a>
+			<a class="btn btn-primary btn-sm update_rate" href="javascript:void(0)" title="Update Rate"><i class="fa fa-cogs">&nbsp;</i>Update Rate</a>
 		<?php endif; ?>
 		<!-- <button type='button' id='update_cost' class="btn btn-sm btn-primary btn-custom">Update Mold</button> -->
 		<span class="pull-right">
@@ -393,6 +394,48 @@ $ENABLE_DELETE  = has_permission('Mold_Rate.Delete');
 				});
 		});
 
+		$(document).on('click', '.update_rate', function() {
+			swal({
+				type: 'warning',
+				title: 'Are you sure ?',
+				text: 'All mold rate will be updated !',
+				showCancelButton: true
+			}, function(next) {
+				if (next) {
+					$.ajax({
+						type: 'type',
+						url: base_url + active_controller + 'update_rate',
+						cache: false,
+						dataType: 'json',
+						success: function(result) {
+							if (result.status == 1) {
+								swal({
+									type: 'success',
+									title: 'Success !',
+									text: result.msg
+								}, function(lanjut) {
+									DataTables();
+								});
+							} else {
+								swal({
+									type: 'warning',
+									title: 'Warning !',
+									text: result.msg
+								});
+							}
+						},
+						error: function(result) {
+							swal({
+								type: 'error',
+								title: 'Error !',
+								text: 'Please try again later !'
+							});
+						}
+					});
+				}
+			});
+		});
+
 		function DataTables() {
 			var table = $('#example1').DataTable({
 				ajax: {
@@ -400,11 +443,10 @@ $ENABLE_DELETE  = has_permission('Mold_Rate.Delete');
 					type: "POST",
 					dataType: "JSON",
 					data: function(d) {
-	
+
 					}
 				},
-				columns: [
-					{
+				columns: [{
 						data: 'no'
 					},
 					{
@@ -480,8 +522,8 @@ $ENABLE_DELETE  = has_permission('Mold_Rate.Delete');
 		function hitung_cost_m3() {
 			var utilisasi_hari = $('input[name="utilisasi_hari"]').val();
 			var utilisasi_m3_per_hari = $('input[name="utilisasi_m3_per_hari"]').val();
-			
-			if(utilisasi_m3_per_hari !== '') {
+
+			if (utilisasi_m3_per_hari !== '') {
 				utilisasi_m3_per_hari = utilisasi_m3_per_hari.split(',').join('');
 				utilisasi_m3_per_hari = parseFloat(utilisasi_m3_per_hari);
 			} else {
@@ -489,7 +531,7 @@ $ENABLE_DELETE  = has_permission('Mold_Rate.Delete');
 			}
 
 			var depresiasi_per_tahun = $('input[name="depresiasi_per_tahun"]').val();
-			if(depresiasi_per_tahun !== '') {
+			if (depresiasi_per_tahun !== '') {
 				depresiasi_per_tahun = depresiasi_per_tahun.split(',').join('');
 				depresiasi_per_tahun = parseFloat(depresiasi_per_tahun);
 			} else {
