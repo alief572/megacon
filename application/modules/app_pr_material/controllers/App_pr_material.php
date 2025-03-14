af9f0cf79b55e2272f4011d6985e1cb331418ff7
syahputra601 <ajisyahputra601@gmail.com>
2025-03-14 13:41:53 +0700

    editing bagian material yang tidak muncul - bagian 4

--- application/modules/app_pr_material/controllers/App_pr_material.php
+++ application/modules/app_pr_material/controllers/App_pr_material.php
@@ -245,7 +245,36 @@ class App_pr_material extends Admin_Controller
         )
         ->result_array();
     } else {
-      $detail     = $this->db
+      // $detail     = $this->db
+      //   // ->select('a.*, b.max_stok, b.min_stok, b.nama AS nm_material')
+      //   // ->join('new_inventory_4 b', 'a.id_material=b.code_lv4', 'left')
+      //   ->select('a.*, c.nama as nm_material, c.max_stok, c.min_stok, b.id_material as material_id')
+      //   ->join('tr_jenis_beton_detail b', 'b.id_detail_material = a.id_material', 'left')
+      //   ->join('new_inventory_4 c', 'b.id_material=c.code_lv4', 'inner')
+      //   ->get_where(
+      //     'material_planning_base_on_produksi_detail a',
+      //     array(
+      //       'a.so_number' => $so_number
+      //     )
+      //   )
+      //   ->result_array();
+      $huruf_pertama = substr($so_number, 0, 1);
+      if($huruf_pertama == 'P'){//UNTUK REQUEST
+        $detail     = $this->db
+        // ->select('a.*, b.max_stok, b.min_stok, b.nama AS nm_material')
+        // ->join('new_inventory_4 b', 'a.id_material=b.code_lv4', 'left')
+        ->select('a.*, c.nama as nm_material, c.max_stok, c.min_stok, b.id_material as material_id')
+        ->join('tr_jenis_beton_detail b', 'b.id_material = a.id_material', 'left')
+        ->join('new_inventory_4 c', 'b.id_material=c.code_lv4', 'inner')
+        ->get_where(
+          'material_planning_base_on_produksi_detail a',
+          array(
+            'a.so_number' => $so_number
+          )
+        )
+        ->result_array();
+      }else{//UNTUK SO //$huruf_pertama == 'S'
+        $detail     = $this->db
         // ->select('a.*, b.max_stok, b.min_stok, b.nama AS nm_material')
         // ->join('new_inventory_4 b', 'a.id_material=b.code_lv4', 'left')
         ->select('a.*, c.nama as nm_material, c.max_stok, c.min_stok, b.id_material as material_id')
@@ -258,6 +287,8 @@ class App_pr_material extends Admin_Controller
           )
         )
         ->result_array();
+      }
+        // echo $this->db->last_query();
     }
 
     $data = [
