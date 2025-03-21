108c579fa38cc716f173277a11b62a643ae6234a
syahputra601 <ajisyahputra601@gmail.com>
2025-03-21 09:18:29 +0700

    editing modul pr material bagian add propose - bagian 4

--- application/modules/request_pr_material/controllers/Request_pr_material.php
+++ application/modules/request_pr_material/controllers/Request_pr_material.php
@@ -266,9 +266,17 @@ class Request_pr_material extends Admin_Controller
       $QTY_PR = NULL;
       //START BAGIAN SET UTO PROPOSE NEW
       $ID_Material = $value['code_lv4'];
+      // $get_qty_stok = $this->db->get_where('warehouse_stock', ['id_material' => $ID_Material])->row();
+      // $Qty_Stok = $get_qty_stok->qty_stock;
+      // $Max_Stok = $value['max_stok'];
+      // $Get_Propose = $Max_Stok - $Qty_Stok;
+      // Mengambil data stok dari database
       $get_qty_stok = $this->db->get_where('warehouse_stock', ['id_material' => $ID_Material])->row();
-      $Qty_Stok = $get_qty_stok->qty_stock;
-      $Max_Stok = $value['max_stok'];
+      // Mengatur nilai default jika qty_stock bernilai null atau kosong
+      $Qty_Stok = isset($get_qty_stok->qty_stock) && $get_qty_stok->qty_stock !== '' ? $get_qty_stok->qty_stock : 0;
+      // Mengatur nilai default jika max_stok bernilai null atau kosong
+      $Max_Stok = isset($row['max_stok']) && $row['max_stok'] !== '' ? $row['max_stok'] : 0;
+      // Menghitung jumlah yang bisa diajukan
       $Get_Propose = $Max_Stok - $Qty_Stok;
       // if ($Get_Propose >= 1000) {
       //     $Get_Propose_fix = number_format($Get_Propose, 0, '.', ','); // Format ribuan dengan koma
